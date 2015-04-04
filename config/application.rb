b983ce5cb90d9a369c78912a7cd6787dd14a47d8
Luke Clewlow <luke.clewlow@gmail.com>
2015-04-04 15:37:48 +0100

    set up routing so that different templates can be served at different time, extracted presentation of tunes list into template

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module Db2
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("templates")
   end
 end
