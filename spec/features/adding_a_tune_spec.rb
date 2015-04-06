2c9d714e267ae7993c7d112e02693bcb1396b278
Luke Clewlow <luke.clewlow@gmail.com>
2015-04-06 10:44:51 +0100

    added new feature test for user input

---
+++ spec/features/adding_a_tune_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'adding a tune to the blog' do
+
+	context 'entering information' do
+		scenario 'should see a box for user input', :js => true do
+			visit('/')
+			expect(page).to have_css('form.form')
+		end
+
+		scenario 'when entering information the video should appear on the page', :js => true do
+			visit('/')
+			fill_in('Artist?', :with => 'Hidden Orchestra')
+			fill_in('Title?', :with => 'Live')
+			fill_in('Blog?', :with =>'Blah')
+			fill_in('Link?', :with =>'https://www.youtube.com/embed/bcE4EaAJr_8')
+			click_button('Add')
+			expect(page).to have_content('Hidden Orchestra')
+		end
+	end
+end
