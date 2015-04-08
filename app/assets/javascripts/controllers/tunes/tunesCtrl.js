d048abfe8ffb69268bfb4fec05970c55ad4a6d77
Luke Clewlow <luke.clewlow@gmail.com>
2015-04-08 22:43:11 +0100

    writing method for posting new tunes into database, currently 400 error, think need to tell Angular to send as JSON type

--- app/assets/javascripts/controllers/tunes/tunesCtrl.js
+++ app/assets/javascripts/controllers/tunes/tunesCtrl.js
@@ -14,17 +14,26 @@ tunesControllers.controller('IndexCtrl', ['$http', '$scope', function($http, $sc
   );
 }]);
 
-tunesControllers.controller('AddTuneCtrl', ['$http', '$scope', function($http, $scope) {
+tunesControllers.controller('AddTunesCtrl', ['$http', '$scope', function($http, $scope) {
   var self = $scope;
 
+  self.tune = {"artist":"a", "title":"a", "blog":"a", "link":"a"};
+  var log = console.log("Loading AddTunesCtrl");
+
   this.add = function() {
-    $http.post('/tunes', self.tune)
-      .then(console.log("IMHERE"))
+    var dataObject = {
+          artist : self.tune.artist
+          ,title  : self.tune.title
+          ,blog : self.tune.blog
+          ,link : self.tune.link
+       };
+    var log = console.log(self.tune);
+    $http(headers: { 'Content-Type': 'application/json' }).post('tunes', dataObject)
       .then(function(response) {
-        self.tune = {};
+          
       });
   };
-}])
+}]);
 
 tunesControllers.controller('GenreCtrl', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams) {
   var self = $scope;
--- app/controllers/tunes_controller.rb
+++ app/controllers/tunes_controller.rb
@@ -10,6 +10,11 @@ class TunesController < ApplicationController
 		respond_with @tunes.to_json
   end
 
+  def show
+    @tune = Tune.find(params[:id])
+    respond_with @tune.to_json
+  end
+
   def create
   	@tune = Tune.create(tune_params)
 		render json: @tune, status: 201
