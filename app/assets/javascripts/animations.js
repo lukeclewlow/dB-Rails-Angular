42f919479051092bd37a95e28d6ce052844cdf12
Luke Clewlow <luke.clewlow@gmail.com>
2015-05-17 09:41:17 +0100

    added dropdown animation and moved menu to be positioned more satisfacoriy

--- app/assets/javascripts/animations.js
+++ app/assets/javascripts/animations.js
@@ -24,4 +24,13 @@ $(document).on('scroll', function(){
 
 $(document).ready(function() {
   $(".navbar-fixed-top").autoHidingNavbar();
+
+  $('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
 });
+
